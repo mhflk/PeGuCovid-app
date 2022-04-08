4c9f5c5fb11479bb9feea91f019325a843f8a243
endhambot <endhambot@gmail.com>
2022-04-08 22:02:02 +0530

    Updates

---
+++ precache-manifest.b49995d7356970a3fb86db26b4564dc3.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52f2d5e969705b8b459107716ac6d92",
+    "url": "/PeGuCovid-app/index.html"
+  },
+  {
+    "revision": "55ead99f43b8fb69ccd6",
+    "url": "/PeGuCovid-app/static/css/2.768d3256.chunk.css"
+  },
+  {
+    "revision": "dcfd2eeb84ab8d65c048",
+    "url": "/PeGuCovid-app/static/css/main.d3ddda6f.chunk.css"
+  },
+  {
+    "revision": "55ead99f43b8fb69ccd6",
+    "url": "/PeGuCovid-app/static/js/2.963147df.chunk.js"
+  },
+  {
+    "revision": "4e876d204123baf7efeb08c673e52e4a",
+    "url": "/PeGuCovid-app/static/js/2.963147df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfd2eeb84ab8d65c048",
+    "url": "/PeGuCovid-app/static/js/main.f7bb9088.chunk.js"
+  },
+  {
+    "revision": "d4e5aea8b0d5eb55277f",
+    "url": "/PeGuCovid-app/static/js/runtime-main.952b7339.js"
+  },
+  {
+    "revision": "9ee4be43ec572ecc1210c540cff40471",
+    "url": "/PeGuCovid-app/static/media/Hermit-Bold.9ee4be43.woff2"
+  },
+  {
+    "revision": "9423a7f2eed82fc0c7c4580e782904bd",
+    "url": "/PeGuCovid-app/static/media/Hermit-BoldItalic.9423a7f2.woff2"
+  },
+  {
+    "revision": "635e121c61e6b2ae1bd2117a9e049b7b",
+    "url": "/PeGuCovid-app/static/media/Hermit-Light.635e121c.woff2"
+  },
+  {
+    "revision": "eb890239ffe31ced209a262723e98c75",
+    "url": "/PeGuCovid-app/static/media/Hermit-LightItalic.eb890239.woff2"
+  },
+  {
+    "revision": "d74c3617da48a886e17ecdd3c68548b8",
+    "url": "/PeGuCovid-app/static/media/Hermit-Regular.d74c3617.woff2"
+  },
+  {
+    "revision": "c6a0928d171eef9e74805dee01b4f587",
+    "url": "/PeGuCovid-app/static/media/Hermit-RegularItalic.c6a0928d.woff2"
+  },
+  {
+    "revision": "8f4b82aa8332273e223a479c8ed512d7",
+    "url": "/PeGuCovid-app/static/media/af.8f4b82aa.png"
+  },
+  {
+    "revision": "913971256fc6daeefc81eb97c4849f3f",
+    "url": "/PeGuCovid-app/static/media/aq.91397125.png"
+  },
+  {
+    "revision": "1d153b2310b64f40acc701b0cfbe5f99",
+    "url": "/PeGuCovid-app/static/media/bl.1d153b23.png"
+  },
+  {
+    "revision": "5305595a5d32ec303e290a0a649f4455",
+    "url": "/PeGuCovid-app/static/media/bt.5305595a.png"
+  },
+  {
+    "revision": "85a3d11dbeeafdcbb0e1f5732458e8d9",
+    "url": "/PeGuCovid-app/static/media/bz.85a3d11d.png"
+  },
+  {
+    "revision": "dbb3305d1cb221f3e67e102ff9cfc257",
+    "url": "/PeGuCovid-app/static/media/cdc-k0KRNtqcjfw-unsplash.dbb3305d.jpg"
+  },
+  {
+    "revision": "c40c8877ea895a4a697bf115d98fc6f2",
+    "url": "/PeGuCovid-app/static/media/ec.c40c8877.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PeGuCovid-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PeGuCovid-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PeGuCovid-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PeGuCovid-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PeGuCovid-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a75a1026c5a7e7c12287c9a8dd9fa5df",
+    "url": "/PeGuCovid-app/static/media/gb-wls.a75a1026.png"
+  },
+  {
+    "revision": "52c5124f5836b9d50c021e2e241c1e66",
+    "url": "/PeGuCovid-app/static/media/gs.52c5124f.png"
+  },
+  {
+    "revision": "55331127951d00909b6d99368293dd95",
+    "url": "/PeGuCovid-app/static/media/io.55331127.png"
+  },
+  {
+    "revision": "f2e03d664db0c7a3a7fa461ea1c05fda",
+    "url": "/PeGuCovid-app/static/media/mp.f2e03d66.png"
+  },
+  {
+    "revision": "64341c41d83316b6b37a3189878d0aec",
+    "url": "/PeGuCovid-app/static/media/pe.64341c41.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/PeGuCovid-app/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "530704a6520fbb9704dffb739d290810",
+    "url": "/PeGuCovid-app/static/media/rs.530704a6.png"
+  },
+  {
+    "revision": "12256dfdc5a6edaea2d0b8914d6e009a",
+    "url": "/PeGuCovid-app/static/media/sm.12256dfd.png"
+  },
+  {
+    "revision": "f4f2ca898742444293f444f139f07f17",
+    "url": "/PeGuCovid-app/static/media/tm.f4f2ca89.png"
+  },
+  {
+    "revision": "9b964ca1babf73ac48b29494388d3a74",
+    "url": "/PeGuCovid-app/static/media/undraw_book_reading_kx9s.9b964ca1.svg"
+  },
+  {
+    "revision": "52529c0b14b1a6278c23d386d810799e",
+    "url": "/PeGuCovid-app/static/media/undraw_cancel_u1it.52529c0b.svg"
+  },
+  {
+    "revision": "2f9d1e244a27e470f0499d5f946b0304",
+    "url": "/PeGuCovid-app/static/media/undraw_conference_call_b0w6.2f9d1e24.svg"
+  },
+  {
+    "revision": "66e903c86923ffdf422ac77ea9e86165",
+    "url": "/PeGuCovid-app/static/media/undraw_cup_of_tea_6nqg.66e903c8.svg"
+  },
+  {
+    "revision": "45a94407e4c5011b827e102bd3f12304",
+    "url": "/PeGuCovid-app/static/media/undraw_doctors_hwty.45a94407.svg"
+  },
+  {
+    "revision": "2f2fb9e5ad7cf0957df760fcdd26862a",
+    "url": "/PeGuCovid-app/static/media/undraw_indoor_bike_pwa4.2f2fb9e5.svg"
+  },
+  {
+    "revision": "7702c99606558483e89f798e20e0bad2",
+    "url": "/PeGuCovid-app/static/media/undraw_medical_research_qg4d.7702c996.svg"
+  },
+  {
+    "revision": "6c2118fcab2747a48a79ff8b1f714ebe",
+    "url": "/PeGuCovid-app/static/media/undraw_online_articles_79ff.6c2118fc.svg"
+  },
+  {
+    "revision": "577eab6a3ecda1954553940134fca049",
+    "url": "/PeGuCovid-app/static/media/undraw_phone_call_grmk.577eab6a.svg"
+  },
+  {
+    "revision": "0af0c1a84f5c0f167cfa5f2800d7014f",
+    "url": "/PeGuCovid-app/static/media/undraw_quality_time_wiyl.0af0c1a8.svg"
+  },
+  {
+    "revision": "d3d6eb5b377f8054e337344edb70a12b",
+    "url": "/PeGuCovid-app/static/media/undraw_reading_time_gvg0.d3d6eb5b.svg"
+  },
+  {
+    "revision": "cce762074f8d2a58bff93d8ff274e4c5",
+    "url": "/PeGuCovid-app/static/media/undraw_remotely_2j6y.cce76207.svg"
+  },
+  {
+    "revision": "b2dc530016e0d373de94c0be4c2296d6",
+    "url": "/PeGuCovid-app/static/media/undraw_social_distancing_2g0u.b2dc5300.svg"
+  },
+  {
+    "revision": "8c121909c25627e426ff7fad694cb8bd",
+    "url": "/PeGuCovid-app/static/media/undraw_wash_hands_nwl2.8c121909.svg"
+  },
+  {
+    "revision": "487addb0a1edd4a30c17e33cc7117098",
+    "url": "/PeGuCovid-app/static/media/va.487addb0.png"
+  },
+  {
+    "revision": "fd18669807e0a2baa0b4f70ca77a0177",
+    "url": "/PeGuCovid-app/static/media/vi.fd186698.png"
+  }
+]);
